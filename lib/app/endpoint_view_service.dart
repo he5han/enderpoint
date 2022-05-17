db6e09540c0ecfae4ef66d21624d01eb398836e0
heshan <heshanto@gmail.com>
2022-05-18 04:31:04 +0530

    chore: refactoring

--- lib/app/endpoint_view_service.dart
+++ lib/app/endpoint_view_service.dart
@@ -1,79 +1,54 @@
-import 'dart:async';
-import 'dart:io';
-
-import 'package:enderpoint/app/endpoint_collection.dart';
 import 'package:rxdart/rxdart.dart';
+
+import 'endpoint_collection.dart';
 
 import '../core/flavor.dart';
 import '../core/endpoint.dart';
-import '../app/endpoint_request_handler.dart';
-
-enum ServerState { closed, idle }
 
 class EndpointViewModel {
   List<Endpoint> endpoints;
-  ServerState state;
+  Endpoint? selectedEndpoint;
 
-  EndpointViewModel(this.endpoints, this.state);
+  EndpointViewModel({required this.endpoints, this.selectedEndpoint});
 }
 
 class EndpointViewService {
   final EndpointCollection _endpointCollection;
 
-  late BehaviorSubject<EndpointViewModel> endpointViewObservable;
-
-  late HttpServer _server;
+  late BehaviorSubject<EndpointViewModel> observable;
   late EndpointViewModel _viewModel;
-  late StreamSubscription _serverSubscription;
 
   EndpointViewService(List<Endpoint> initialEndpoints) : _endpointCollection = EndpointCollection(initialEndpoints);
-
-  _notifyEndpointObservable(EndpointViewModel model) {
-    endpointViewObservable.add(model);
-  }
 
   setFlavor(Endpoint endpoint, Flavor flavor) {
     endpoint.flavor = flavor;
     _viewModel.endpoints = _endpointCollection.list;
-    _notifyEndpointObservable(_viewModel);
+    observable.add(_viewModel);
   }
 
   addEndpoint(Endpoint endpoint) {
     _endpointCollection.list.add(endpoint);
     _viewModel.endpoints = _endpointCollection.list;
-    _notifyEndpointObservable(_viewModel);
+    observable.add(_viewModel);
   }
 
   removeEndpoint(Endpoint endpoint) {
     _endpointCollection.list.remove(endpoint);
     _viewModel.endpoints = _endpointCollection.list;
-    _notifyEndpointObservable(_viewModel);
+    observable.add(_viewModel);
   }
 
-  start() async {
-    try {
-      EndpointRequestHandler requestHandler = EndpointRequestHandler(_endpointCollection);
-      _serverSubscription = _server.listen(requestHandler.handleHttpRequest, onDone: () => stop());
-    } on Error {
-      _server = await HttpServer.bind(InternetAddress.anyIPv4, 8080, shared: true);
-      await start();
-    }
-
-    _viewModel.state = ServerState.idle;
-    _notifyEndpointObservable(_viewModel);
-
-    return;
-  }
-
-  stop() async {
-    await _serverSubscription.cancel();
-    await _server.close(force: true);
-    _viewModel.state = ServerState.closed;
-    _notifyEndpointObservable(_viewModel);
+  setSelectedEndpoint(Endpoint endpoint) {
+    _viewModel.selectedEndpoint = endpoint;
+    observable.add(_viewModel);
   }
 
   init() {
-    _viewModel = EndpointViewModel([], ServerState.closed);
-    endpointViewObservable = BehaviorSubject<EndpointViewModel>.seeded(_viewModel);
+    _viewModel = EndpointViewModel(endpoints: []);
+    observable = BehaviorSubject<EndpointViewModel>.seeded(_viewModel);
+  }
+
+  EndpointViewModel getCurrentSync() {
+    return _viewModel;
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
-import './core/flavor.dart';
-import './core/endpoint.dart';
+import 'dart:math';
 
-import './app/endpoint_collection.dart';
-import './app/endpoint_request_handler.dart';
-import './app/endpoint_view_service.dart';
+import 'package:enderpoint/app/endpoint_server_view_service.dart';
+
+import 'core/flavor.dart';
+import 'core/endpoint.dart';
+
+import 'app/endpoint_collection.dart';
+import 'app/endpoint_request_handler.dart';
+import 'app/endpoint_view_service.dart';
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -15,13 +19,21 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  List<Provider> getProviders() {
+    EndpointCollection collection = EndpointCollection([]);
+    EndpointRequestHandler requestHandler = EndpointRequestHandler(collection);
+
+    return [
+      Provider<EndpointViewService>(create: (_) => EndpointViewService(collection.list)..init()),
+      Provider<EndpointServerViewService>(
+          create: (_) => EndpointServerViewService(EndpointServerHandler(requestHandler))..init())
+    ];
+  }
+
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [
-        Provider<EndpointViewService>(
-            create: (_) => EndpointViewService([])..init())
-      ],
+      providers: getProviders(),
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
@@ -47,9 +59,37 @@ class _MyHomePageState extends State<MyHomePage> {
     super.initState();
   }
 
+  Endpoint createRandomEndpoint(List<Endpoint> endpoints) {
+    int rNum = Random().nextInt(100);
+    //
+    // Endpoint tmp = Endpoint(
+    //     rNum,
+    //     [
+    //       Flavor(identifier: 0x00, body: {"msg": "hello world"}, statusCode: 200),
+    //       Flavor(identifier: 0x02, body: {"msg": "Error"}, statusCode: 500)
+    //     ],
+    //     "/test-$rNum");
+    //
+    // if (endpoints.contains(tmp)) {
+    //   tmp = createRandomEndpoint(endpoints);
+    // }
+    //
+    // return tmp;
+
+    return Endpoint(
+        rNum,
+        [
+          Flavor(identifier: 0x00, body: {"msg": "hello world"}, statusCode: 200),
+          Flavor(identifier: 0x02, body: {"msg": "Error"}, statusCode: 500),
+        ],
+        "/test-$rNum");
+  }
+
   @override
   Widget build(BuildContext context) {
-    EndpointViewService viewService = Provider.of<EndpointViewService>(context);
+    EndpointViewService endpointViewService = Provider.of<EndpointViewService>(context);
+    EndpointServerViewService endpointServerViewService = Provider.of<EndpointServerViewService>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -59,44 +99,41 @@ class _MyHomePageState extends State<MyHomePage> {
         children: <Widget>[
           TextButton(
               onPressed: () {
-                viewService.start();
-              },
-              child: const Text("Start server")),
-          TextButton(
-              onPressed: () {
-                viewService.stop();
-              },
-              child: const Text("Destroy server")),
-          TextButton(
-              onPressed: () {
-                viewService.addEndpoint(Endpoint(
-                    0xff,
-                    [
-                      Flavor(identifier: 0x00, body: {"msg": "hello world"}, statusCode: 200),
-                      Flavor(identifier: 0x02, body: {"msg": "Error"}, statusCode: 500)
-                    ],
-                    "/test"));
+                endpointViewService.addEndpoint(createRandomEndpoint(endpointViewService.getCurrentSync().endpoints));
               },
               child: const Text("Add Endpoint")),
+          StreamBuilder<EndpointServerViewModel>(
+            stream: endpointServerViewService.observable,
+            builder: (context, snapshot) => Visibility(
+              visible: snapshot.data != null && snapshot.data!.state == ServerState.closed,
+              child: TextButton(
+                  onPressed: () {
+                    endpointServerViewService.start();
+                  },
+                  child: const Text("Start server")),
+              replacement: TextButton(
+                  onPressed: () {
+                    endpointServerViewService.stop();
+                  },
+                  child: const Text("Destroy server")),
+            ),
+          ),
           Expanded(
             child: StreamBuilder<EndpointViewModel>(
-                stream: viewService.endpointViewObservable,
+                stream: endpointViewService.observable,
                 builder: (context, snapshot) {
                   if (snapshot.hasData) {
-                    return Column(
-                      children: [
-                        Text(snapshot.data!.state.toString()),
-                        ListView(
-                          children: (snapshot.data!.endpoints)
-                              .map((endpoint) => EndpointCard(
-                                    endpoint: endpoint,
-                                    onFlavorSelect: (flavor) => viewService.setFlavor(endpoint, flavor),
-                                    onRemove: () => viewService.removeEndpoint(endpoint),
-                                  ))
-                              .toList(),
-                          shrinkWrap: true,
-                        ),
-                      ],
+                    return ListView(
+                      children: (snapshot.data!.endpoints)
+                          .map((endpoint) => EndpointCard(
+                                endpoint: endpoint,
+                                isSelected: snapshot.data!.selectedEndpoint == endpoint,
+                                onSelect: () => endpointViewService.setSelectedEndpoint(endpoint),
+                                onFlavorSelect: (flavor) => endpointViewService.setFlavor(endpoint, flavor),
+                                onRemove: () => endpointViewService.removeEndpoint(endpoint),
+                              ))
+                          .toList(),
+                      shrinkWrap: true,
                     );
                   }
                   return const Text(":)");
@@ -110,31 +147,48 @@ class _MyHomePageState extends State<MyHomePage> {
 
 class EndpointCard extends StatelessWidget {
   final Endpoint endpoint;
+  final bool isSelected;
+
   final Function(Flavor) onFlavorSelect;
-  final Function onRemove;
-  const EndpointCard({Key? key, required this.endpoint, required this.onFlavorSelect, required this.onRemove})
+  final Function() onRemove;
+  final Function() onSelect;
+
+  const EndpointCard(
+      {Key? key,
+      required this.endpoint,
+      required this.onFlavorSelect,
+      required this.onRemove,
+      required this.onSelect,
+      required this.isSelected})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          children: [
-            Expanded(child: Text(endpoint.url)),
-            TextButton(onPressed: () => onRemove(), child: const Text("x"))
-          ],
-        ),
-        Row(
-            children: endpoint.flavors
-                .map((e) => TextButton(
-                    onPressed: () => onFlavorSelect(e),
-                    child: Text(
-                      e.statusCode.toString(),
-                      style: TextStyle(color: e == endpoint.flavor ? Colors.black : Colors.grey),
-                    )))
-                .toList())
-      ],
+    return Container(
+      child: Column(
+        children: [
+          GestureDetector(
+            onTap: () => onSelect(),
+            child: Row(
+              children: [
+                Expanded(child: Text(endpoint.url)),
+                TextButton(onPressed: () => onRemove(), child: const Text("x"))
+              ],
+            ),
+          ),
+          Row(
+              children: endpoint.flavors
+                  .map((e) => TextButton(
+                      onPressed: () => onFlavorSelect(e),
+                      child: Text(
+                        e.statusCode.toString(),
+                        style: TextStyle(color: e == endpoint.flavor ? Colors.black : Colors.grey),
+                      )))
+                  .toList())
+        ],
+      ),
+      decoration: BoxDecoration(
+          border: Border.all(width: 1, color: isSelected ? Theme.of(context).primaryColor : Colors.transparent)),
     );
   }
 }
